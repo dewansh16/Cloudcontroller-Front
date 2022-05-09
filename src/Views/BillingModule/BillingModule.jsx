b3905e76d0addd5a5f9cecb73ad7e4b0de6fd7a9
Cuong <cuong.nguyen@ncubelabs.com>
2022-05-09 15:32:45 +0700

    integrate api for code 99453

--- src/Views/BillingModule/BillingModule.jsx
+++ src/Views/BillingModule/BillingModule.jsx
@@ -30,6 +30,7 @@ import "./billingModule.css";
 
 import billingApi from "../../Apis/billingApis";
 import tenantApi from "../../Apis/tenantApis";
+import {CPT_CODE, CPT} from "../../Utils/utils";
 
 const { Panel } = Collapse;
 const { TextArea } = Input;
@@ -220,9 +221,6 @@ function BillingModule() {
         setRunUseEffect(temp);
     }
 
-    console.log('runUseEffect', runUseEffect);
-    console.log('initialSetupState', initialSetupState);
-
     function findDateIndex(month) {
         return month === currentActiveMonth;
     }
@@ -301,57 +299,36 @@ function BillingModule() {
     }
 
     function initialSetupPost() {
-        console.log('---------------------------');
-        console.log("PATCH INFO : ", patchArray);
-        console.log("PATCH INFO : ", patchInformation);
-
         var date = new Date();
         var date_string = date.toISOString();
-
-        // billingApi
-        //     .addBillingTask(
-        //         {
-        //             code_type: "CPTR",
-        //             code: "919457",
-        //             status: "99453",
-        //             bill_date: date_string,
-        //             pid: location.state.pid,
-        //             code_tasks: [
-        //                 {
-        //                     Billing_Information: [
-        //                         {
-        //                             code: "99453",
-        //                             code_internal: "",
-        //                             useruuid: "",
-        //                             date_time: date_string,
-        //                             taskTotalTimeSpent: "0",
-        //                             timeConsidered: "0",
-        //                             task: "initial_setup_complete",
-        //                             status: true,
-        //                         },
-        //                     ],
-        //                     Patch_Patient_Information: patchInformation,
-        //                     Patches: { patch_list: patchArray },
-        //                 },
-        //             ],
-        //         }
-        //     )
-        //     .then((res) => {
-        //         console.log("BILLING RESPONSE : ", res);
-        //         var temp = runUseEffect;
-        //         temp = temp + 1;
-        //         setRunUseEffect(temp);
-        //     })
-        //     .catch((err) => {
-        //         console.log(err);
-        //     });
-
         setInitialSetupLoading(false);
-        var temp = runUseEffect;
-        temp = temp + 1;
-        setRunUseEffect(temp);
+        billingApi
+            .addBillingTask(
+                {
+                    code_type: CPT,
+                    code: CPT_CODE.CPT_99453,
+                    bill_date: date_string,
+                    pid: location.state.pid,
+                    time_spent: 20,
+                    revenue_code: 123,
+                    notecodes: "pending",
+                    bill_process: 0,
+                    fee: 20
+
+                }
+            )
+            .then((res) => {
+                console.log("BILLING RESPONSE : ", res);
+                var temp = runUseEffect;
+                temp = temp + 1;
+                setRunUseEffect(temp);
+                setInitialSetupState(true);
+            })
+            .catch((err) => {
+                console.log(err);
+                setInitialSetupState(true);
+            });
         
-        setInitialSetupState(true);
     }
 
     function enrollForPatch() {
@@ -453,11 +430,11 @@ function BillingModule() {
             billingApi
                 .getBillingTasks(location.state.pid, currentDateApi, '0')
                 .then((res) => {
-                    console.log("BILLING RESPONSE : ", res.data.response.billingData[0]);
+                    console.log("BILLING RESPONSE : ", res.data.response.billingData);
                     // setFirstTwentyTasks(res.data.response.billingData[0].code_tasks)
                     setTasksLoadingState(false);
 
-                    setBillingInformation(res.data.response.billingData[0].code_tasks[0].Billing_Information)
+                    setBillingInformation(res.data.response.billingData)
 
                     setTenantuuid(res.data.response.billingData[0].tenant_id);
 
@@ -482,7 +459,7 @@ function BillingModule() {
                     var firstTotalTime = 0;
                     var secondTotalTimeStageOne = 0;
                     var secondTotalTimeStageTwo = 0;
-                    res.data.response.billingData[0].code_tasks[0].Billing_Information.map(
+                    res.data.response.billingData.map(
                         (item) => {
                             tempDataSource.push({
                                 date: getDateFromISO(item.date_time),
@@ -491,16 +468,16 @@ function BillingModule() {
                                 duration: getMinutesFromSeconds(item.timeConsidered),
                             });
 
-                            if (item.code === "99453") {
-                                const d = new Date(item.date_time);
+                            if (item.code == CPT_CODE.CPT_99453) {
+                                const d = new Date(item.bill_date);
                                 initialStepDone = true;
                                 initialData = {
-                                    date: getDateFromISO(item.date_time),
-                                    time: getTimeFromISO(item.date_time),
+                                    date: getDateFromISO(item.bill_date),
+                                    time: getTimeFromISO(item.bill_date),
                                     month: monthNames[d.getMonth()],
                                 };
                             }
-                            if (item.code === "99454") {
+                            if (item.code == "99454") {
                                 enrollPatch = true;
                                 tempPatchdata = {
                                     date: getDateFromISO(item.date_time),
@@ -517,8 +494,8 @@ function BillingModule() {
                                     };
                                 }
                             }
-                            if (item.code === "99458") {
-                                if (item.code_internal === "99458_stage1") {
+                            if (item.code == "99458") {
+                                if (item.code_internal == "99458_stage1") {
                                     tempSecondTwentyTasks.push(item);
                                     secondTotalTimeStageOne =
                                         secondTotalTimeStageOne + Number(item.timeConsidered);
@@ -529,7 +506,7 @@ function BillingModule() {
                                         };
                                     }
                                 }
-                                if (item.code_internal === "99458_stage2") {
+                                if (item.code_internal == "99458_stage2") {
                                     tempSecondTwentyStageTwoTasks.push(item);
                                     secondTotalTimeStageTwo =
                                         secondTotalTimeStageTwo + Number(item.timeConsidered);
@@ -1443,7 +1420,7 @@ function BillingModule() {
                     // setFirstTwentyTasks(res.data.response.billingData[0].code_tasks)
                     setTasksLoadingState(false);
 
-                    setBillingInformation(res.data.response.billingData[0].code_tasks[0].Billing_Information)
+                    setBillingInformation(res.data.response.billingData)
 
                     setTenantuuid(res.data.response.billingData[0].tenant_id);
 
@@ -1468,34 +1445,34 @@ function BillingModule() {
                     var firstTotalTime = 0;
                     var secondTotalTimeStageOne = 0;
                     var secondTotalTimeStageTwo = 0;
-                    res.data.response.billingData[0].code_tasks[0].Billing_Information.map(
+                    res.data.response.billingData.map(
                         (item) => {
                             tempDataSource.push({
-                                date: `${getDateFromISO(item.date_time)} ${getTimeFromISO(
-                                    item.date_time
+                                date: `${getDateFromISO(item.bill_date)} ${getTimeFromISO(
+                                    item.bill_date
                                 )}`,
                                 code: item.code,
-                                desc: item.task,
+                                desc: '',
                                 duration: getMinutesFromSeconds(item.timeConsidered),
                             });
 
-                            if (item.code === "99453") {
-                                const d = new Date(item.date_time);
+                            if (item.code == CPT_CODE.CPT_99453) {
+                                const d = new Date(item.bill_date);
                                 initialStepDone = true;
                                 initialData = {
-                                    date: getDateFromISO(item.date_time),
-                                    time: getTimeFromISO(item.date_time),
+                                    date: getDateFromISO(item.bill_date),
+                                    time: getTimeFromISO(item.bill_date),
                                     month: monthNames[d.getMonth()],
                                 };
                             }
-                            if (item.code === "99454") {
+                            if (item.code == CPT_CODE.CPT_99454) {
                                 enrollPatch = true;
                                 tempPatchdata = {
                                     date: getDateFromISO(item.date_time),
                                     time: getTimeFromISO(item.date_time),
                                 };
                             }
-                            if (item.code === "99457") {
+                            if (item.code == CPT_CODE.CPT_99457) {
                                 tempFirstTwentyTasks.push(item);
                                 firstTotalTime = firstTotalTime + Number(item.timeConsidered);
                                 if (!tempFirstTwentyData.hasOwnProperty("date")) {
@@ -1505,7 +1482,7 @@ function BillingModule() {
                                     };
                                 }
                             }
-                            if (item.code === "99458") {
+                            if (item.code == CPT_CODE.CPT_99458) {
                                 if (item.code_internal === "99458_stage1") {
                                     tempSecondTwentyTasks.push(item);
                                     secondTotalTimeStageOne =
@@ -1546,9 +1523,7 @@ function BillingModule() {
                     if (initialStepDone) {
                         setInitialBillDate(res.data.response.billingData[0].bill_date)
                         setEnrolledState(true);
-                        if (taskDeleteArray.length === 0) {
-                            setInitialSetupState(true);
-                        }
+                        setInitialSetupState(true);
                         setInitialSetupData(initialData);
                     }
                     setInitialSetupLoading(false);
