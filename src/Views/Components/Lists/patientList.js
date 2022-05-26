f924591aee5ba6737131eb64e71a196794b3675a
trieunguyenterra <trieu.nguyen@terralogic.com>
2022-05-26 09:43:01 +0700

    [LIV-169] update get data from sensor

--- src/Views/Components/Lists/patientList.js
+++ src/Views/Components/Lists/patientList.js
@@ -76,38 +76,38 @@ const PatientListItem = (props) => {
     const [isChartSectionVisible, setChartSectionVisibility] = React.useState(false);
 
     const arrDataChart = [
-        // {
-        //     _key: 'temp',
-        //     name: "Temperature",
-        //     icon: Icons.thermometerIcon({ Style: { color: Colors.purple } }),
-        //     val: 0,
-        //     color: Colors.purple,
-        //     trendData: []
-        // },
-        // {
-        //     _key: 'spo2',
-        //     name: "SPO2",
-        //     icon: Icons.o2({ Style: { color: Colors.green } }),
-        //     val: 0,
-        //     color: Colors.green,
-        //     trendData: []
-        // },
-        // {
-        //     _key: 'ecg_hr',
-        //     name: "Heart Rate",
-        //     icon: Icons.ecgIcon({ Style: { color: Colors.darkPink } }),
-        //     val: 0,
-        //     color: Colors.darkPink,
-        //     trendData: []
-        // },
-        // {
-        //     _key: 'ecg_rr',
-        //     name: "Respiration Rate",
-        //     icon: Icons.lungsIcon({ Style: { color: Colors.orange } }),
-        //     val: 0,
-        //     color: Colors.orange,
-        //     trendData: []
-        // },
+        {
+            _key: 'temp',
+            name: "Temperature",
+            icon: Icons.thermometerIcon({ Style: { color: Colors.purple } }),
+            val: 0,
+            color: Colors.purple,
+            trendData: []
+        },
+        {
+            _key: 'spo2',
+            name: "SPO2",
+            icon: Icons.o2({ Style: { color: Colors.green } }),
+            val: 0,
+            color: Colors.green,
+            trendData: []
+        },
+        {
+            _key: 'ecg_hr',
+            name: "Heart Rate",
+            icon: Icons.ecgIcon({ Style: { color: Colors.darkPink } }),
+            val: 0,
+            color: Colors.darkPink,
+            trendData: []
+        },
+        {
+            _key: 'ecg_rr',
+            name: "Respiration Rate",
+            icon: Icons.lungsIcon({ Style: { color: Colors.orange } }),
+            val: 0,
+            color: Colors.orange,
+            trendData: []
+        },
         {
             _key: "blood_pressuer",
             name: "Blood Pressure",
@@ -119,16 +119,16 @@ const PatientListItem = (props) => {
             color: Colors.darkPurple,
             trendData: []
         },
-        // {
-        //     _key: 'weight',
-        //     name: "Weight",
-        //     icon: Icons.bpIcon({
-        //         Style: { color: Colors.yellow, fontSize: "24px" },
-        //     }),
-        //     val: 0,
-        //     color: Colors.yellow,
-        //     trendData: []
-        // },
+        {
+            _key: 'weight',
+            name: "Weight",
+            icon: Icons.bpIcon({
+                Style: { color: Colors.yellow, fontSize: "24px" },
+            }),
+            val: 0,
+            color: Colors.yellow,
+            trendData: []
+        },
     ]
 
     const [chartBlockData, setChartBlockData] = React.useState(arrDataChart);
