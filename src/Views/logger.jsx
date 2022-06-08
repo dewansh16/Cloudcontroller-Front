f94adb302760751bdf5c3c303a25251c6084ca2a
trieunguyenterra <trieu.nguyen@terralogic.com>
2022-06-08 09:04:23 +0700

    create logger

---
+++ src/Views/logger.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { io } from "socket.io-client";
+
+import { Input } from "antd";
+const { TextArea } = Input;
+
+const Logger = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        var socket = io('http://20.230.234.202:7124', { transports: ['websocket', 'polling', 'flashsocket'] });
+
+        socket.on("SENSOR_LOG", function ({ body }) {
+            console.log("---------------------- socket", body);
+            data.push(body);
+            setData([...data]);
+        })
+    }, []);
+
+    return (
+        <div style={{ padding: "2rem", height: "100vh" }}>
+            <div style={{
+                border: "1px solid #ff9452",
+                height: "100%",
+                borderRadius: "8px",
+            }}>
+                {data.map((item, index) => {
+                    return (
+                        <div key={index} style={{ padding: "0.5rem 3rem", borderBottom: "1px solid #ddd", wordBreak: "break-all", overflow: "auto" }}>
+                            {`${index+1}: ${JSON.stringify(item)}`}
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    );
+}
+
+export default Logger;
