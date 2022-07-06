bc8b0dd7620f3af2a9763834667ec10233361ef3
Cuong <cuong.nguyen@ncubelabs.com>
2022-07-06 15:18:42 +0700

    UI for add note care dashboard

--- src/Views/CareDashboard/component/BulbIcon/index.jsx
+++ src/Views/CareDashboard/component/BulbIcon/index.jsx
@@ -1,14 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IconBulbOn from "../../../../Assets/Images/bulbOn.png"
 import IconBulbOff from "../../../../Assets/Images/bulbOff.png";
 import { Button, Input, Popover } from 'antd';
+import billingApi from '../../../../Apis/billingApis';
+import {CPT_CODE} from '../../../../Utils/utils';
+import {isObject, isArray} from 'lodash';
 
-const BulbIcon = () => {
+const BulbIcon = ({pid, billDate, onSaveNoteToDb}) => {
     const [isTurnOn, setIsTurnOn] = useState(false);
     const [arrayNote, setArrayNote] = useState([]);
+    const [item99453, setItem99453] = useState(null);
     const [valueInput, setValInput] = useState("");
 
+    const getArrNoteData = () => {
+        billingApi.getBillingTasks(pid, billDate, '0').then((res) => {
+            res.data.response.billingData.map(item => {
+                if(item.code == CPT_CODE.CPT_99453){
+                    try{
+                        let params = JSON.parse(item.params);
+                        if(!isArray(params)){
+                            params = [];
+                        }
+                        setArrayNote(params);
+                        setItem99453(item)
+                    } catch(err){
+                        console.log(err)
+                    }
+                }
+            })
+        }).catch((err) => {
+            console.log(err);
+        });
+    } 
+
+    useEffect(() => {
+        getArrNoteData();
+    }, [])
     return (
         <Popover
             title="Add Note"
@@ -16,25 +44,62 @@ const BulbIcon = () => {
             trigger={["click"]}
             content={
                 <div>
-                    {arrayNote?.map((note, index) => {
+                    {arrayNote?.map((item, index) => {
                         return (
-                            <div key={index} style={{ marginBottom: "8px" }}>{note}</div>
+                            <div key={index} style={{ marginBottom: "8px" }}>
+                                <p>{item?.note}</p>
+                                <div>
+                                    {item?.status == 1 && (
+                                        <p>Done</p>
+                                    )}
+                                    {item?.status == 0 && (
+                                        <Button 
+                                        type='primary' 
+                                        style={{ fontSize: "12px", marginLeft: "0.5rem" }}
+                                        onClick={() => {
+                                            const dataUpdate = {
+                                                note: item.note,
+                                                status: 1,
+                                                billing_id: item99453.id,
+                                                code: item99453.code,
+                                                bill_date: item99453.bill_date,
+                                                pid: pid,
+                                                task_id: item.task_id
+                                            }
+                                            onSaveNoteToDb(dataUpdate);
+                                        }}
+                                    >
+                                        Complete
+                                    </Button>
+                                    )}
+                                </div>
+                            </div>
                         )
                     })}
-                    <div style={{ display: "flex", alignItems: "center" }}>
+                    {isObject(item99453) && (
+                        <div style={{ display: "flex", alignItems: "center" }}>
                         <Input value={valueInput} onChange={(event) => setValInput(event?.target?.value)} />
                         <Button 
                             type='primary' 
                             style={{ fontSize: "12px", marginLeft: "0.5rem" }}
                             onClick={() => {
-                                const newArr = [...arrayNote, valueInput];
-                                setArrayNote([...newArr]);
+                                const dataUpdate = {
+                                    note: valueInput,
+                                    status: 0,
+                                    billing_id: item99453.id,
+                                    code: item99453.code,
+                                    bill_date: item99453.bill_date,
+                                    pid: pid
+                                }
+                                onSaveNoteToDb(dataUpdate);
                                 setValInput("");
                             }}
                         >
                             Add
                         </Button>
                     </div>
+                    )}
+                    
                 </div>
             }
         >
